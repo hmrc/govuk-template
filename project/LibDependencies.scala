579b6a87a50c60e1864a9c5b778aefabb979b1a3
Dane Watts <danewatts97@gmail.com>
2019-09-11 17:19:36 +0100

    X-Compilation fix for play-26 and scala 2.12 (#74)
    * X compilation (#73)

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -12,12 +12,21 @@ object LibDependencies {
       )
     )
 
-  val test: Seq[ModuleID] = Seq(
-    "org.scalatest" %% "scalatest" % "3.0.5" % Test,
-    "org.pegdown"   %  "pegdown"   % "1.6.0" % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % Test,
-    "org.jsoup" % "jsoup" % "1.12.1" % Test
-  )
+  val test: Seq[ModuleID] =
+    PlayCrossCompilation.dependencies(
+      play25 = Seq(
+        "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+        "org.pegdown"   %  "pegdown"   % "1.6.0" % Test,
+        "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % Test,
+        "org.jsoup" % "jsoup" % "1.12.1" % Test
+      ),
+      play26 = Seq(
+        "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+        "org.pegdown"   %  "pegdown"   % "1.6.0" % Test,
+        "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
+        "org.jsoup" % "jsoup" % "1.12.1" % Test
+      )
+    )
 
   // downgrade twirl for play 2.5 compatibility
   val overrides: Set[ModuleID] =
