be9b21c2b500dbb05646256ed24e0f1e7940055e
James Rees <6393840+Jxr227@users.noreply.github.com>
2019-09-11 11:20:46 +0100

    Add welsh language support for footer and report current lang in the html attribute (#71)
    * WELSH: Add Welsh language support

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -2,34 +2,28 @@ import sbt.{ModuleID, _}
 
 object LibDependencies {
 
-  val compile: Seq[ModuleID] =
-    PlayCrossCompilation.dependencies(
-      play25 = Seq(
-        "com.typesafe.play" %% "play" % "2.5.19" % "provided"
-      ),
-      play26 = Seq(
-        "com.typesafe.play" %% "play" % "2.6.20" % "provided"
-      )
+  val compile: Seq[ModuleID] = PlayCrossCompilation.dependencies(
+    play25 = Seq(
+      "com.typesafe.play" %% "play" % "2.5.19" % "provided"
+    ),
+    play26 = Seq(
+      "com.typesafe.play" %% "play" % "2.6.20" % "provided"
     )
+  )
 
-  val test: Seq[ModuleID] =
-    PlayCrossCompilation.dependencies(
-      play25 = Seq(
-        "org.scalatest" %% "scalatest" % "3.0.5" % Test,
-        "org.pegdown"   %  "pegdown"   % "1.6.0" % Test,
-        "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % Test,
-        "org.jsoup" % "jsoup" % "1.12.1" % Test
-      ),
-      play26 = Seq(
-        "org.scalatest" %% "scalatest" % "3.0.5" % Test,
-        "org.pegdown"   %  "pegdown"   % "1.6.0" % Test,
-        "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
-        "org.jsoup" % "jsoup" % "1.12.1" % Test
-      )
+  val test: Seq[ModuleID] = PlayCrossCompilation.dependencies(
+    play25 = Seq(
+      "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+      "org.pegdown"   %  "pegdown"   % "1.6.0" % Test,
+      "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % Test,
+      "org.jsoup" % "jsoup" % "1.12.1" % Test
+    ),
+    play26 = Seq(
+      "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+      "org.pegdown"   %  "pegdown"   % "1.6.0" % Test,
+      "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test,
+      "org.jsoup" % "jsoup" % "1.12.1" % Test
     )
-    Seq(
-    "org.scalatest" %% "scalatest" % "3.0.5" % Test,
-    "org.pegdown"   %  "pegdown"   % "1.6.0" % Test
   )
 
   // downgrade twirl for play 2.5 compatibility
