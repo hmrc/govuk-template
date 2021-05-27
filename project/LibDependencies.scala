fd4ae9611f7993460653b3341a035a7f2a5df8a1
Steven Truong <45595406+struong@users.noreply.github.com>
2021-05-27 17:04:48 +0100

    DDCNL-3943: Moved to flexmark

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -18,18 +18,18 @@ object LibDependencies {
   val test: Seq[ModuleID] =
     PlayCrossCompilation.dependencies(
       shared = Seq(
-        "org.scalatest"          %% "scalatest"          % "3.0.8"    % Test,
-        "org.pegdown"            %  "pegdown"            % "1.6.0"    % Test,
-        "org.jsoup"              % "jsoup"               % "1.12.1"   % Test
+        "org.scalatest" %% "scalatest" % "3.0.8" % Test,
+        "org.jsoup" % "jsoup" % "1.12.1" % Test,
+        "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test
       ),
       play26 = Seq(
-        "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"    % Test
+        "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
       ),
       play27 = Seq(
-        "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3"    % Test
+        "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % Test
       ),
       play28 = Seq(
-        "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"    % Test
+        "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
       )
     )
 }
