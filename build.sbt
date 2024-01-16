d798b1ee6a24f94ed5c453ad1c9af5ca6ad46a86
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-01-16 17:14:45 +0000

    PLATUI-2716: First attempt for Play 2.9

--- build.sbt
+++ build.sbt
@@ -13,8 +13,7 @@ lazy val root = Project("govuk-template-play-29", file("."))
       routesGenerator    := InjectedRoutesGenerator,
       (sourceDirectories in (Compile, TwirlKeys.compileTemplates)) += baseDirectory.value / "src/main/twirl"
       ,
-      excludeFilter.in(unmanagedResources.in(headerCreate)) := "*.mustache.html", // don't add licence headers to mustache templates
-      PlayCrossCompilation.playCrossCompilationSettings
+      excludeFilter.in(unmanagedResources.in(headerCreate)) := "*.mustache.html" // don't add licence headers to mustache templates
   )
   .settings(unmanagedResourceDirectories in sbt.Compile += baseDirectory.value / "resources")
   .disablePlugins(sbt.plugins.JUnitXmlReportPlugin)
--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -7,10 +7,10 @@ object LibDependencies {
   )
 
   val test =  Seq(
-        "org.scalatest" %% "scalatest" % "3.0.8" % Test,
-        "org.jsoup" % "jsoup" % "1.12.1" % Test,
-        "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test,
-        "org.pegdown" % "pegdown" % "1.6.0" % Test,
-        "org.scalatestplus.play" %% "scalatestplus-play"    % "6.0.0"       % Test
-      )
+    "org.scalatest"          %% "scalatest"          % "3.0.8"  % Test,
+    "org.jsoup"               % "jsoup"              % "1.12.1" % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"       % "0.64.8" % Test,
+    "org.pegdown"             % "pegdown"            % "1.6.0"  % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "6.0.0"  % Test
+  )
 }
