ef0b652278cfcb33f73c3b6fff826b3aa05178b3
Joanna Pinto Paul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-01-17 11:26:12 +0000

    Plat UI 2716 remove cross compilation (#130)
    PLATUI-2716: Removed PlayCrossCompilation, new major version for Play 2.9

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -2,35 +2,14 @@ import sbt.{ModuleID, _}
 
 object LibDependencies {
 
-  val compile: Seq[ModuleID] =
-    PlayCrossCompilation.dependencies(
-      play26 = Seq(
-        "com.typesafe.play" %% "play" % "2.6.20" % "provided"
-      ),
-      play27 = Seq(
-        "com.typesafe.play" %% "play" % "2.7.4" % "provided"
-      ),
-      play28 = Seq(
-        "com.typesafe.play" %% "play" % "2.8.7" % "provided"
-      )
-    )
+  val compile: Seq[ModuleID] = Seq(
+    "com.typesafe.play" %% "play" % "2.9.0"
+  )
 
-  val test: Seq[ModuleID] =
-    PlayCrossCompilation.dependencies(
-      shared = Seq(
-        "org.scalatest" %% "scalatest" % "3.0.8" % Test,
-        "org.jsoup" % "jsoup" % "1.12.1" % Test,
-        "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test,
-        "org.pegdown" % "pegdown" % "1.6.0" % Test
-      ),
-      play26 = Seq(
-        "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
-      ),
-      play27 = Seq(
-        "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % Test
-      ),
-      play28 = Seq(
-        "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
-      )
-    )
+  val test: Seq[ModuleID] = Seq(
+    "org.scalatest"          %% "scalatest"          % "3.2.17" % Test,
+    "org.jsoup"               % "jsoup"              % "1.13.1" % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"       % "0.64.8" % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "6.0.0"  % Test
+  )
 }
